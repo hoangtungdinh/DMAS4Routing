5e6feadf1d3e890caa1e96d2d1cbf57caf383e39
Hoang Tung Dinh <tunghdinh@gmail.com>
2015-03-25 01:25:32 +0100

    Contract Net Protocol Example (still error codes)

--- DMas4Routing/src/main/java/cNetProtocol/AGVAgent.java
+++ DMas4Routing/src/main/java/cNetProtocol/AGVAgent.java
@@ -1,9 +1,12 @@
 package cNetProtocol;
 
+import java.util.List;
+
 import com.github.rinde.rinsim.core.TimeLapse;
 import com.github.rinde.rinsim.core.model.comm.CommDevice;
 import com.github.rinde.rinsim.core.model.comm.CommDeviceBuilder;
 import com.github.rinde.rinsim.core.model.comm.CommUser;
+import com.github.rinde.rinsim.core.model.comm.Message;
 import com.github.rinde.rinsim.core.model.pdp.PDPModel;
 import com.github.rinde.rinsim.core.model.pdp.Vehicle;
 import com.github.rinde.rinsim.core.model.road.RoadModel;
@@ -13,21 +16,71 @@ import com.google.common.base.Optional;
 public class AGVAgent extends Vehicle implements CommUser {
   
   private static final double SPEED = 1000d;
-  private Optional<RoadModel> roadModel;
-  private Optional<PDPModel> pdpModel;
+  Optional<RoadModel> roadModel;
+  Optional<PDPModel> pdpModel;
   Optional<CommDevice> device;
   private double range = 2.0;
   private double reliability = 1.0;
+  private State state = State.IDLE;
+  private Package parcel = null;
+  
+  public AGVAgent(Point startPosition) {
+    setStartPosition(startPosition);
+    setCapacity(3.0);
+    roadModel = Optional.absent();
+    pdpModel = Optional.absent();
+  }
 
   @Override
   public double getSpeed() {
     return SPEED;
   }
 
+
   @Override
   protected void tickImpl(TimeLapse time) {
-    // TODO Auto-generated method stub
-    
+    if (state == State.IDLE) {
+      if (device.get().getUnreadCount() != 0) {
+        List<Message> messageList = device.get().getUnreadMessages();
+        for (Message message : messageList) {
+          if (message.getContents() instanceof TaskAnnouncements) {
+            CommUser sender = message.getSender();
+            device.get().send(new TaskBid(this.getPosition()), sender);
+            state = State.WAITING;
+            break;
+          }
+        }
+      }
+    } else if (state == State.WAITING) {
+      List<Message> messageList = device.get().getUnreadMessages();
+      for (Message message : messageList) {
+        if (message.getContents() instanceof TaskAward) {
+          TaskAward taskAward = (TaskAward) message.getContents();
+          if (taskAward.getAward()) {
+            state = State.WORKING;
+            parcel = (Package) message.getSender();
+          } else {
+            state = State.IDLE;
+          }
+        }
+      }
+    } else {
+      if (parcel != null) {
+        final boolean inCargo = pdpModel.get().containerContains(this, parcel);
+        
+        if (!inCargo) {
+          roadModel.get().moveTo(this, parcel, time);
+          if (roadModel.get().equalPosition(this, parcel)) {
+            pdpModel.get().pickup(this, parcel, time);
+          }
+        } else {
+          roadModel.get().moveTo(this, parcel.getDestination(), time);
+          if (roadModel.get().getPosition(this).equals(parcel.getDestination())) {
+            pdpModel.get().deliver(this, parcel, time);
+          }
+        }
+      }
+    }
   }
 
   @Override
@@ -49,6 +102,10 @@ public class AGVAgent extends Vehicle implements CommUser {
     device = Optional.of(builder
         .setReliability(reliability)
         .build());
+  }
+  
+  enum State {
+    IDLE, WAITING, WORKING
   }
 
 }
--- DMas4Routing/src/main/java/cNetProtocol/CNetExample.java
+++ DMas4Routing/src/main/java/cNetProtocol/CNetExample.java
@@ -7,12 +7,10 @@ import com.github.rinde.rinsim.core.TickListener;
 import com.github.rinde.rinsim.core.TimeLapse;
 import com.github.rinde.rinsim.core.model.comm.CommModel;
 import com.github.rinde.rinsim.core.model.pdp.DefaultPDPModel;
-import com.github.rinde.rinsim.core.model.pdp.PDPModel;
-import com.github.rinde.rinsim.core.model.road.MovingRoadUser;
 import com.github.rinde.rinsim.core.model.road.PlaneRoadModel;
-import com.github.rinde.rinsim.core.model.road.RoadModel;
 import com.github.rinde.rinsim.geom.Point;
 import com.github.rinde.rinsim.ui.View;
+import com.github.rinde.rinsim.ui.renderers.PDPModelRenderer;
 import com.github.rinde.rinsim.ui.renderers.PlaneRoadModelRenderer;
 import com.github.rinde.rinsim.ui.renderers.RoadUserRenderer;
 
@@ -21,6 +19,8 @@ public final class CNetExample {
   static final double VEHICLE_SPEED_KMH = 50d;
   static final Point MIN_POINT = new Point(0, 0);
   static final Point MAX_POINT = new Point(10, 10);
+  static final int NUM_AGV = 5;
+  static final int NUM_PARCEL = 10;
 
   private CNetExample() {}
 
@@ -39,5 +39,43 @@ public final class CNetExample {
      final DefaultPDPModel pdpModel = DefaultPDPModel.create();
      
      final CommModel commModel = CommModel.builder().build();
+     
+     final Simulator simulator = Simulator.builder()
+         .addModel(planeModel)
+         .addModel(pdpModel)
+         .addModel(commModel)
+         .build();
+     
+     final RandomGenerator rng = simulator.getRandomGenerator();
+     
+     for (int i = 0; i < NUM_AGV; i++) {
+       simulator.register(new AGVAgent(planeModel.getRandomPosition(rng)));
+     }
+    for (int i = 0; i < NUM_PARCEL; i++) {
+      simulator.register(new Package(planeModel.getRandomPosition(rng),
+          planeModel.getRandomPosition(rng), 1, 1, 1 + rng.nextInt(3)));
+    }
+    
+    simulator.addTickListener(new TickListener() {
+      @Override
+      public void tick(TimeLapse time) {
+        if (rng.nextDouble() < .007) {
+          simulator.register(new Package(planeModel.getRandomPosition(rng),
+              planeModel.getRandomPosition(rng), 1, 1, 1 + rng.nextInt(3)));
+        }
+      }
+
+      @Override
+      public void afterTick(TimeLapse timeLapse) {
+      }
+    });
+    
+    final View.Builder viewBuilder = View.create(simulator)
+        .with(PlaneRoadModelRenderer.create())
+        .with(RoadUserRenderer.builder())
+        .with(new PDPModelRenderer());
+    
+    
+    viewBuilder.show();
   }
 }
