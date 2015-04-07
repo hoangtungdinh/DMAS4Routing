a64087fa11778ef88f14631017db1907e29bb6ac
Hoang Tung Dinh <tunghdinh@gmail.com>
2015-04-07 18:08:14 +0200

    Remove deadlock warning

--- DMas4Routing/src/main/java/delegateMAS/Setting.java
+++ DMas4Routing/src/main/java/delegateMAS/Setting.java
@@ -3,10 +3,10 @@ package delegateMAS;
 public class Setting {
   
   public static final double VEHICLE_LENGTH = 2d;
-  public static final int MAP_SIZE_X = 4;
-  public static final int MAP_SIZE_Y = 4;
-  public static final int NUM_AGENTS = 15;
-  public static final int BLOCK_SIZE = 1;
+  public static final int MAP_SIZE_X = 32;
+  public static final int MAP_SIZE_Y = 32;
+  public static final int NUM_AGENTS = 30;
+  public static final int BLOCK_SIZE = 4;
   public static final long TIME_WINDOW = 10000;
   public static final int PHEROMONES_LIFE_TIME = 10;
 
