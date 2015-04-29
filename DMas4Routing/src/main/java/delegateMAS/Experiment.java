31df968c87b7e79bc42dffad0940276f26dc86fa
Hoang Tung Dinh <tunghdinh@gmail.com>
2015-04-29 15:06:29 +0200

    add actionfailure experiment

--- DMas4Routing/src/main/java/delegateMAS/Experiment.java
+++ DMas4Routing/src/main/java/delegateMAS/Experiment.java
@@ -15,7 +15,7 @@ public class Experiment {
   public static void main(String[] args) {
     final int intention = 1;
     final int numOfRuns = 10;
-    final String fileName = "-Map2Random-";
+    final String fileName = "-ActionFailtureLatticeMap-";
     
     List<Result> resultList = new ArrayList<>();
     List<Double> successRatio = new ArrayList<>();
